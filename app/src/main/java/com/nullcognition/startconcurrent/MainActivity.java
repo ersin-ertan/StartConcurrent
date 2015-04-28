6c665c7374f69650f8e40d1f421bedcfc6a74e0a
ersin <ersin_the_ertan@hotmail.com>
2015-04-27 20:35:07 -0400

    Read up to chapter13, which starts concurrent programming.

--- app/src/main/java/com/nullcognition/startconcurrent/MainActivity.java
+++ app/src/main/java/com/nullcognition/startconcurrent/MainActivity.java
@@ -1,9 +1,14 @@
 package com.nullcognition.startconcurrent;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -13,23 +18,64 @@ public class MainActivity extends ActionBarActivity {
 
 	  super.onCreate(savedInstanceState);
 	  setContentView(R.layout.activity_main);
+
+	  Log.e(getClass().getSimpleName(), Integer.toString(dnaSearchRegEx())); // all methods indicate 1, but there are two
+	  Log.e(getClass().getSimpleName(), Integer.toString(dnaSearch()));
+	  Log.e(getClass().getSimpleName(), Integer.toString(dnaSearchFor()));
+
+
    }
+
+   public int dnaSearchRegEx(){
+
+	  Pattern p = Pattern.compile("(.)*TATTA(.)*");
+	  Matcher m = p.matcher("TATTATTAGATTA");
+
+	  int count = 0;
+	  while(m.find()){ ++ count; }
+	  return count;
+   }
+
+   public int dnaSearch(){
+
+	  Scanner s = new Scanner("TATTATTAGATTA");
+
+	  int count = 0;
+	  while(s.hasNext("(.)*TATTA(.)*")){
+		 ++ count;
+		 s.next("(.)*TATTA(.)*");
+	  }
+	  return count;
+   }
+
+   public int dnaSearchFor(){
+
+	  String sub = "TATTA";
+	  String input = "TATTATTAGATTA";
+
+	  int count = 0;
+
+	  while(input.contains(sub)){
+		 ++ count;
+		 input = input.replace(sub, "");
+	  }
+
+	  return count;
+   }
+
 
    @Override
    public boolean onCreateOptionsMenu(Menu menu){
-	  // Inflate the menu; this adds items to the action bar if it is present.
+
 	  getMenuInflater().inflate(R.menu.menu_main, menu);
 	  return true;
    }
 
    @Override
    public boolean onOptionsItemSelected(MenuItem item){
-	  // Handle action bar item clicks here. The action bar will
-	  // automatically handle clicks on the Home/Up button, so long
-	  // as you specify a parent activity in AndroidManifest.xml.
+
 	  int id = item.getItemId();
 
-	  //noinspection SimplifiableIfStatement
 	  if(id == R.id.action_settings){
 		 return true;
 	  }
